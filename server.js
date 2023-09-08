895c105218d07f21b5e89fdd1bd92da554f95ec9
Tristenh <tristenhobba1@gmail.com>
2023-09-08 19:17:05 +1000

    added server.js installed dependencies, initialized handlebars and express

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const session = require("express-session");
+const routes = require("./controllers");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+// create instance of handlebars engine
+const hbs = exphbs.create({});
+const app = express();
+const PORT = process.env.PORT || 3001;
+// configure handlebars as the default template engine
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+// serve static files
+app.use(express.static(path.join(__dirname, "public")));
+global.__basedir = __dirname;
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+app.use(session(sess));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+// handling unmatched routes and serving  404
+app.get("*", (req, res) => {
+  res.status(404).send("page not found");
+});
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
