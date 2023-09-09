ce472308ec2fd6951493bead327b7e3a03ae3c86
Tristenh <tristenhobba1@gmail.com>
2023-09-09 23:03:09 +1000

    added date_created to comment seeds, in post:id route including user in comment

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,85 +1,89 @@
-const router = require('express').Router();
-const {Post, User, Comment} = require('../models')
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/profile", withAuth, async (req, res) => {
-    try {
-      // Find the logged in user based on the session ID
-      const userData = await User.findByPk(req.session.user_id, {
-        attributes: { exclude: ["password"] },
-        include: [{ model: Post }],
-      });
-      const user = userData.get({ plain: true });
-      res.render("profile", {
-        ...user,
-        logged_in: true,
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
-  router.get("/login", (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.logged_in) {
-      res.redirect("/profile");
-      return;
-    }
-    res.render("login");
-  });
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
+    const user = userData.get({ plain: true });
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+  res.render("login");
+});
 
 //   get all posts
-router.get('/', async (req, res) => {
-    try {
-      // Get all Posts and JOIN with user data
-      const postData = await Post.findAll({
-        include: [
-          {
-            model: User,
-            attributes: ['name'],
-          },
-          {
-            model: Comment,
-            attributes: ['id'],
-          },
-        ],
-      });
-  
-      // Serialize data so the template can read it
-      const posts = postData.map((post) => post.get({ plain: true }));
-  
-      // Pass serialized data and session flag into template
-      res.render('homepage', { 
-        posts, 
-        logged_in: req.session.logged_in 
-      });
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.get("/", async (req, res) => {
+  try {
+    // Get all Posts and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+        {
+          model: Comment,
+          attributes: ["id"],
+        },
+      ],
+    });
 
-  // get Posts by id
-router.get('/post/:id', withAuth, async (req, res) => {
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// get Posts by id
+router.get("/post/:id", withAuth, async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
-
+          attributes: ["name"],
         },
         {
           model: Comment,
-          attributes: ['id', 'commentdesc', 'post_id', 'user_id'],
+          attributes: ["id", "commentdesc", "post_id", "user_id", "date_created"],
+          include: [
+            {
+              model: User,
+              attributes: ["name"],
+            },
+          ],
         },
       ],
     });
-
     const post = postData.get({ plain: true });
-
-    res.render('post', {
+    console.log(post);
+    res.render("post", {
       ...post,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -6,28 +6,33 @@ const commentData = [
       "Great tutorial! I'm excited to get started with CMS blogging.",
     post_id: 1,
     user_id: 1,
+    date_created: new Date(),
   },
   {
     commentdesc:
       "I've been using Handlebars.js for my projects, and it's amazing!",
     post_id: 2,
     user_id: 2,
+    date_created: new Date(),
   },
   {
     commentdesc:
       "User authentication is crucial for security. Thanks for the insights!",
     post_id: 3,
     user_id: 3,
+    date_created: new Date(),
   },
   {
     commentdesc:
       "SEO optimization is essential for online visibility. Great tips!",
     post_id: 4,
     user_id: 4,
+    date_created: new Date(),
   },
 ];
 
 // Bulk create the comments
-const seedComments = () => Comment.bulkCreate(commentData, { individualHooks: true });
+const seedComments = () =>
+  Comment.bulkCreate(commentData, { individualHooks: true });
 // Export the seedComments function
 module.exports = seedComments;
