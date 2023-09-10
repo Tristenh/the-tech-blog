57948eef48b27f51f87c62bd5b80544ed9e7a084
Tristenh <tristenhobba1@gmail.com>
2023-09-10 15:39:46 +1000

    added destroy Post by id, added fetch delete on the targeted buttons post.id,  rename createPost to api/postRoute and js/createpodt to post for better naming

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const commentRoutes = require("./commentRoute");
-const createpost = require("./createpost");
+const commentRoutes = require("./commentroute");
+const postRoute = require("./postRoute");
 
 router.use("/users", userRoutes);
 router.use("/comments", commentRoutes);
-router.use("/createpost", createpost);
+router.use("/posts", postRoute);
 
 module.exports = router;
--- public/js/post.js
+++ public/js/post.js
@@ -1,18 +1,15 @@
 const addProject = async (event) => {
   event.preventDefault();
-
   // Collect values from the comment-add text area
   const title = document.querySelector("#project-name").value.trim();
   const description = document.querySelector("#project-desc").value.trim();
-
   if (title && description) {
     // Send a POST request to the API endpoint
-    const response = await fetch("/api/createpost", {
+    const response = await fetch("/api/posts", {
       method: "POST",
       body: JSON.stringify({ title, description }),
       headers: { "Content-Type": "application/json" },
     });
-
     if (response.ok) {
       // If successful, reload page
       console.log("added a post");
@@ -22,6 +19,22 @@ const addProject = async (event) => {
     }
   }
 };
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to delete project");
+    }
+  }
+};
 document
   .querySelector(".new-project-form")
   .addEventListener("submit", addProject);
+document
+  .querySelector(".posts-list")
+  .addEventListener("click", delButtonHandler);
