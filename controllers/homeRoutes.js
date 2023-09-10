e443b4a185e9f49e2a063c66a71898afdc6001d0
Tristenh <tristenhobba1@gmail.com>
2023-09-10 12:10:28 +1000

    styles post.handlebars, added redner layputs:layouts to profile page, layouts.handlebars changes h1 to Dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,6 +11,7 @@ router.get("/profile", withAuth, async (req, res) => {
     });
     const user = userData.get({ plain: true });
     res.render("profile", {
+      layout: 'layouts',
       ...user,
       logged_in: true,
     });
